ffb7a57245a7a200060bb42457df4f2a16620299
23gprochazka <23gprochazka@dgkralupy.eu>
2022-02-13 13:40:43 +0100

    add heading blocks

--- packages/lib/src/ui/blocks/enums.ts
+++ packages/lib/src/ui/blocks/enums.ts
@@ -2,6 +2,8 @@ export enum BlockTemplates {
   Header = "header",
   GroupList = "groupList",
   GalleryList = "galleryList",
+  Heading1 = "heading1",
+  Heading2 = "heading2",
 }
 
 export enum InputType {
