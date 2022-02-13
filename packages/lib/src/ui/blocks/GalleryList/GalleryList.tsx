f010d8a2b8528f749dade46496aed6acf27e7489
smutnjo <smutnjo2@fit.cvut.cz>
2022-02-13 11:32:10 +0100

    add GalleryList block

--- packages/lib/src/ui/blocks/GalleryList/GalleryList.tsx
+++ packages/lib/src/ui/blocks/GalleryList/GalleryList.tsx
@@ -3,26 +3,17 @@ import { Container } from "../../components/Container/Container"
 import styled from "@emotion/styled"
 import { Header3, SmallText } from "../../components/Typography/Typography"
 import React from "react"
-import { GalleryProps } from "./galleryDef"
-
-import Rectangle_1 from "../../assets/vectors/potatoes/rectangle_1.svg"
-import Rectangle_2 from "../../assets/vectors/potatoes/rectangle_2.svg"
-import Rectangle_3 from "../../assets/vectors/potatoes/rectangle_3.svg"
-import Rectangle_4 from "../../assets/vectors/potatoes/rectangle_4.svg"
+import { GalleryListProps } from "./galleryListDef"
 import { max, theme } from "../../styles/theme"
+import { FixedHeightImage } from "../../components/FixedHeightImage/FixedHeightImage"
 
-const rectangles = [Rectangle_1, Rectangle_2, Rectangle_3, Rectangle_4]
-
-export const Gallery: React.FC<GalleryProps> = (props) => (
+export const GalleryList: React.FC<GalleryListProps> = (props) => (
   <Block>
     <Container>
       <GalleryViewContainer>
         {props.content.map((galleryView, index) => (
           <GalleryView key={index} href={galleryView.address}>
-            <Picture
-              src={galleryView.image.src}
-              mask={rectangles[(index * 13 + 11) % 4].src}
-            />
+            <FixedHeightImage src={galleryView.image.src} />
             <Title>{galleryView.name}</Title>
             <Date>{galleryView.comment}</Date>
           </GalleryView>
@@ -43,31 +34,22 @@ const GalleryView = styled.a`
   display: flex;
   flex-direction: column;
   max-width: 100vw;
-`
 
-const Picture = styled.img`
-  width: auto;
-  mask-image: url("${(props: { mask: string }) => props.mask}");
-  mask-size: 100% 100%;
-  mask-position: center;
-  align-self: center;
-  height: 35vh;
-
-  @media ${max("l")} {
-    height: 30vh;
-  }
-
-  @media ${max("s")} {
-    height: auto;
-    width: 90vw;
+  img {
+    border-radius: 1rem;
   }
 `
+
 const Title = styled(Header3)`
   color: ${theme.color.darkest};
   text-align: center;
   max-width: 40vw;
   align-self: center;
   margin: 0.3em 0 0;
+
+  @media ${max("m")} {
+    max-width: 80vw;
+  }
 `
 
 const Date = styled(SmallText)`
