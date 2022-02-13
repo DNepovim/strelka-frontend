72e171a683344bf6374e2e2e2b41c68f09b74c2a
Dominik Bláha <dominik.blaha@avast.com>
2022-02-13 09:18:41 +0100

    Fix build

--- packages/admin/api/createPage.ts
+++ packages/admin/api/createPage.ts
@@ -1,20 +1,21 @@
+import { Pages, PagesClient } from "@strelka-skaut/js-api-client"
 import { PageWithContent } from "../schemas/page"
 
-// const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT
+const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT
 
 export const createPage = async (values: PageWithContent) => {
-  // if (!apiEndpoint) {
-  //   throw new Error("NEXT_PUBLIC_API_ENDPOINT is not defined")
-  // }
-  // try {
-  //   const createPageRquest = new Pages.CreatePageRequest()
-  //   createPageRquest.setName(values.name)
-  //   createPageRquest.setSlug(values.slug)
-  //   createPageRquest.setContent(JSON.stringify(values.content))
-  //   const client = new PagesClient.ServiceClient(apiEndpoint)
-  //   const response = await client.createPage(createPageRquest, {})
-  //   return { id: response.getId() }
-  // } catch (e) {
-  //   throw e
-  // }
+  if (!apiEndpoint) {
+    throw new Error("NEXT_PUBLIC_API_ENDPOINT is not defined")
+  }
+  try {
+    const createPageRquest = new Pages.CreatePageRequest()
+    createPageRquest.setName(values.name)
+    createPageRquest.setSlug(values.slug)
+    createPageRquest.setContent(JSON.stringify(values.content))
+    const client = new PagesClient.ServiceClient(apiEndpoint)
+    const response = await client.createPage(createPageRquest, {})
+    return { id: response.getId() }
+  } catch (e) {
+    throw e
+  }
 }
