267be3279a0c8c6f3a0c320d36607cc089fc0632
José Smutný <smutnjo2@fit.cvut.cz>
2022-05-19 19:54:20 +0200

    tweak gallery list style

--- packages/lib/src/ui/blocks/GalleryList/GalleryList.tsx
+++ packages/lib/src/ui/blocks/GalleryList/GalleryList.tsx
@@ -4,8 +4,8 @@ import styled from "@emotion/styled"
 import { Header3, SmallText } from "../../components/Typography/Typography"
 import React from "react"
 import { GalleryListProps } from "./galleryListDef"
-import { max, theme } from "../../styles/theme"
-import { Image } from "../../components/Image/Image"
+import { min, theme } from "../../styles/theme"
+import { FreeformImage } from "../../components/FreeformImage/FreeformImage"
 
 export const GalleryList: React.FC<GalleryListProps> = (props) => (
   <Block>
@@ -23,15 +23,29 @@ export const GalleryList: React.FC<GalleryListProps> = (props) => (
   </Block>
 )
 
-const GalleryViewContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 2rem;
+const calculateGalleryImageSide = (
+  columnCount: number,
+  gapWidth: number,
+  totalWidth: number
+) => (totalWidth - (columnCount + 1) * gapWidth) / columnCount
+// + 1 for additional space on sides
 
-  @media ${max("m")} {
-    flex-direction: column;
-    flex-wrap: nowrap;
+const GalleryViewContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.25rem 2vw;
+
+  padding: 0 2vw;
+
+  @media ${min("m")} {
+    grid-template-columns: repeat(3, 1fr);
+    padding: 0 2.5vw;
+    gap: 2.5vw;
+  }
+
+  @media ${min("l")} {
+    padding: 0 1.5rem;
+    gap: 1.5rem;
   }
 `
 
@@ -39,17 +53,25 @@ const GalleryView = styled.a`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  &:hover h3 {
+    text-decoration: underline;
+  }
 `
 
-const GalleryImage = styled(Image)`
-  height: 20em;
+const GalleryImage = styled(FreeformImage)`
   img {
-    border-radius: 1rem;
+    border-radius: 0.75rem;
   }
 
-  @media ${max("m")} {
-    height: auto;
-    width: 90%;
+  height: ${calculateGalleryImageSide(2, 2, 100)}vw;
+
+  @media ${min("m")} {
+    height: ${calculateGalleryImageSide(3, 2, 100)}vw;
+  }
+
+  @media ${min("l")} {
+    height: ${calculateGalleryImageSide(3, 1.5, theme.layout.width)}rem;
   }
 `
 
@@ -58,10 +80,14 @@ const Title = styled(Header3)`
   text-align: center;
   align-self: center;
   max-width: 15em;
-  margin: 0.3em 0 0;
+  font-size: ${theme.size.base * 1.1}rem;
+  line-height: 1em;
+  margin: 0.4rem 0 0.2rem;
 
-  @media ${max("m")} {
-    max-width: 20em;
+  @media ${min("m")} {
+    line-height: 1.1em;
+    font-size: ${theme.size.base * 1.3}rem;
+    margin: 0.75rem 0 0.2rem;
   }
 `
 
