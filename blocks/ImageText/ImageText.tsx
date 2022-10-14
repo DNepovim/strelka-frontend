7466564f8b0d364c5850aaed4464bbab0e72b908
Dominik Bláha <dominik.blaha@avast.com>
2022-10-15 00:49:28 +0200

    Add icons to image text blocks

--- blocks/ImageText/ImageText.tsx
+++ blocks/ImageText/ImageText.tsx
@@ -5,10 +5,35 @@ import { Column, Container, Row } from "../../components/Layout/Layout"
 import { Header2, Text } from "../../components/Typography/Typography"
 import SquareMask from "../../assets/vectors/potatoes/rectangle_5.svg"
 import styled from "@emotion/styled"
+import Bed from "../../assets/vectors/icons/bed.svg"
+import Pig from "../../assets/vectors/icons/pig.svg"
+import Marker from "../../assets/vectors/icons/marker.svg"
+import Person from "../../assets/vectors/icons/person.svg"
+import Case from "../../assets/vectors/icons/case.svg"
+import Image from "next/image"
+
+export enum Icons {
+  Bed = "bed",
+  Pig = "pig",
+  Marker = "marker",
+  Person = "person",
+  Case = "case",
+}
+
+export const icons = {
+  [Icons.Bed]: Bed,
+  [Icons.Pig]: Pig,
+  [Icons.Marker]: Marker,
+  [Icons.Person]: Person,
+  [Icons.Case]: Case,
+}
 
 export interface ImageTextProps extends BlockFields {
   title?: string
-  text?: string[]
+  text?: {
+    icon?: Icons
+    text?: string[]
+  }[]
   imageUrl?: string
 }
 
@@ -24,13 +49,23 @@ export const ImageText: React.FC<ImageTextProps> = ({
       </Row>
       <Row>
         <Column col={12} m={6}>
-          <Text>
-            {text?.map((line) => (
-              <>
-                {line} <br />
-              </>
-            ))}
-          </Text>
+          {text?.map(({ icon, text }) => (
+            <TextRow>
+              {icon && (
+                <Icon>
+                  <Image src={icons[icon]} />
+                </Icon>
+              )}
+              <Text>
+                {text.map((t) => (
+                  <>
+                    {t}
+                    <br />
+                  </>
+                ))}
+              </Text>
+            </TextRow>
+          ))}
         </Column>
         <Column col={12} m={6}>
           {imageUrl && (
@@ -49,6 +84,18 @@ export const ImageText: React.FC<ImageTextProps> = ({
     </Container>
   </Block>
 )
+
+const TextRow = styled.div`
+  display: flex;
+`
+
+const Icon = styled.div`
+  flex: 0 0 2em;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0 2em 0.6em 0;
+`
 
 const CenteredImage = styled.div`
   max-width: 30rem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,29 +3,15 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import { BlockTemplates } from "../blocks/BlockTemplates"
 import { Header } from "../blocks/Header/Header"
-import { ImageText } from "../blocks/ImageText/ImageText"
+import { Icons, ImageText, ImageTextProps } from "../blocks/ImageText/ImageText"
 import { Column, Container, Layout, Row } from "../components/Layout/Layout"
 import { GlobalStyles } from "../components/GlobaStyles/GlobalStyles"
 import { MetaTags } from "../components/MetaTags/MetaTags"
 import { Header1 } from "../components/Typography/Typography"
-import { linkData, personData } from "../mockData/mockData"
+import { building, contacts, linkData, personData } from "../mockData/mockData"
 import { theme } from "../styles/theme"
 import { Page } from "../types/Page"
 import { Block } from "../components/Block/Block"
-
-const data = {
-  title: "Fakturační údaje",
-  text: [
-    "Junák – český skaut,",
-    "středisko Střelka Kralupy nad Vltavo u , z. s.",
-    "Šafaříkova 358, 278 01, Kralupy n. Vlt.",
-    "vedená u Městského soudu v Praze",
-    "",
-    "2107489044/2700 (hlavní účet)",
-    "2801050859/2010 (pro platby akcí)",
-  ],
-  imageUrl: "https://picsum.photos/520/350",
-}
 
 const Home: NextPage<Props> = ({ page }) => (
   <div>
@@ -48,7 +34,6 @@ const Home: NextPage<Props> = ({ page }) => (
     </Head>
 
     <GlobalStyles />
-
     <Layout>
       <Header {...page.blocks[0].fields} />
       <Block>
@@ -60,7 +45,8 @@ const Home: NextPage<Props> = ({ page }) => (
           </Row>
         </Container>
       </Block>
-      <ImageText {...data} />
+      <ImageText {...page.blocks[1].fields} />
+      <ImageText {...page.blocks[2].fields} />
     </Layout>
   </div>
 )
@@ -71,33 +57,15 @@ export const getServerSideProps: () => { props: Props } = () => ({
     page: {
       blocks: [
         {
-          id: "",
-          template: BlockTemplates.Header,
           fields: {
             content: linkData,
           },
         },
         {
-          id: "",
-          template: BlockTemplates.Heading2,
-          fields: {
-            content: { content: "Vedení" },
-          },
+          fields: contacts,
         },
         {
-          id: "",
-          template: BlockTemplates.PersonList,
-          fields: {
-            content: personData,
-          },
-        },
-
-        {
-          id: "",
-          template: BlockTemplates.Heading2,
-          fields: {
-            content: { content: "Fotogalerie" },
-          },
+          fields: building,
         },
       ],
     },
