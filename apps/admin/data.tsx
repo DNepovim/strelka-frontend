d92f8cd6d1fee2eb8fc46a0e471f127d605acc88
Dominik Bláha <dominik.blaha@avast.com>
2022-10-17 19:02:49 +0200

    Remove blind urls from main nav

--- apps/admin/data.tsx
+++ apps/admin/data.tsx
@@ -8,15 +8,14 @@ import {
   IoCalendarOutline,
 } from "react-icons/io5"
 
-
 export const adminNavigation: Navigation = [
   {
     title: "Sekce",
-    slug: "sekce",
+    slug: "",
     icon: <IoBrowsersOutline />,
     action: {
       title: "Vytvořit novou",
-      slug: "sekce/vytvorit",
+      slug: "",
       icon: <IoAddOutline />,
     },
   },
@@ -27,7 +26,7 @@ export const adminNavigation: Navigation = [
   },
   {
     title: "Nastavení",
-    slug: "nastaveni",
+    slug: "",
     icon: <IoSettingsOutline />,
   },
 ]
@@ -45,11 +44,11 @@ export const navigation: Navigation = [
   },
   {
     title: "Události",
-    slug: "udalosti",
+    slug: "",
     icon: <IoCalendarOutline />,
     action: {
       title: "Vytvořit novou",
-      slug: "udalosti/vytvorit",
+      slug: "",
       icon: <IoAddOutline />,
     },
   },
@@ -60,7 +59,7 @@ export const navigation: Navigation = [
   },
   {
     title: "Nastavení",
-    slug: "nastaveni",
+    slug: "",
     icon: <IoSettingsOutline />,
   },
 ]
@@ -72,55 +71,3 @@ export interface PagesTableItem {
   lastEditedTime: string
   lastEditedBy: string
 }
-
-export const pages: PagesTableItem[] = [
-  {
-    title: "Hlavní stránka",
-    slug: "hlavni-stranka",
-    id: "kkdfjlskdf",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "Kontakty",
-    slug: "kontakty",
-    id: "kkdfjlsdie",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "O nás",
-    slug: "o-nas",
-    id: "kkdfdf",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "Kariera",
-    slug: "kariera",
-    id: "dfjlskdf",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "Příběh",
-    slug: "pribeh",
-    id: "kkdfjlsgebvl",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "Další",
-    slug: "dalsi",
-    id: "kkdfjlf",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-  {
-    title: "Kolo",
-    slug: "kolo",
-    id: "kkdfjdf",
-    lastEditedTime: "6. 2. 2022 11:10:10",
-    lastEditedBy: "nik@skaut.cz",
-  },
-]
