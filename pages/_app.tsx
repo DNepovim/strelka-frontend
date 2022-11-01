474f820ffef5efd78f86d216e3c0decea742e02a
José Smutný <smutnjo2@fit.cvut.cz>
2022-11-01 23:42:53 +0100

    extract common navigation to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,65 @@
 import type { AppProps } from "next/app"
+import Head from "next/head"
+import { MetaTags } from "../components/MetaTags/MetaTags"
+import { theme } from "../styles/theme"
 import { GlobalStyles } from "../components/GlobalStyles/GlobalStyles"
+import { Layout } from "../components/Layout/Layout"
+import { Header, HeaderProps } from "../blocks/Header/Header"
+import {
+  GroupHeader,
+  GroupHeaderProps,
+} from "../blocks/GroupHeader/GroupHeader"
+import { Footer } from "../blocks/Footer/Footer"
+import { VerticalSpace } from "../blocks/VerticalSpace/VerticalSpace"
+
+export const BaseComponentWrapper: React.FC<{ headerData: HeaderProps }> = (
+  props
+) => (
+  <>
+    <Header {...props.headerData} />
+    {props.children}
+    <VerticalSpace height={3} />
+    <Footer {...props.headerData} />
+  </>
+)
+
+export const GroupComponentWrapper: React.FC<{
+  headerData: GroupHeaderProps
+}> = (props) => (
+  <>
+    <GroupHeader {...props.headerData} />
+    {props.children}
+    <VerticalSpace height={3} />
+    <Footer items={props.headerData.links} />
+  </>
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <MetaTags
+          title={""}
+          description={""}
+          brandColor={theme.color.lightAccent}
+          themeColor={theme.color.darkAccent}
+          url={""}
+          image=""
+          manifest=""
+          icons={{
+            appleTouchIcon: "",
+            largeIcon: "",
+            smallIcon: "",
+            maskIcon: "",
+          }}
+        />
+      </Head>
+      <GlobalStyles />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  )
 }
 
 export default MyApp
