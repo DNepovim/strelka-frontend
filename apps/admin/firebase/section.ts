8526ec5be12d878ff9f9021c75d95e31fba5ee9e
Dominik Bláha <dominik.blaha@avast.com>
2023-01-14 19:35:58 +0100

    Load list of users sections new way

--- apps/admin/firebase/section.ts
+++ apps/admin/firebase/section.ts
@@ -1,3 +1,5 @@
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { db } from "./db"
 import { getDoc, getDocsList, removeDoc, updateDoc } from "./docs"
 import { getUser } from "./user"
 
@@ -24,10 +26,23 @@ export const getSectionsList = async (email?: string): Promise<Section[]> => {
     return await getDocsList<Section>(collectionName)()
   }
 
-  const sectionsSlugs = [
-    ...(user.roles?.admin ?? []),
-    ...(user.roles?.editor ?? []),
-  ]
 
-  return Promise.all(sectionsSlugs.map(async (slug) => await getSection(slug)))
+  const col = collection(db, collectionName)
+  const admins = await query(
+    col,
+    where(`roles.admins`, "array-contains", email)
+  )
+  const editors = await query(
+    col,
+    where(`roles.editors`, "array-contains", email)
+  )
+  const adminsDocs = await getDocs(admins)
+  const editorsDocs = await getDocs(editors)
+  return await [...adminsDocs.docs, ...editorsDocs.docs].map((item) => {
+    const data = item.data() as Omit<Section, "slug">
+    return {
+      slug: item.id,
+      ...data,
+    }
+  })
 }
